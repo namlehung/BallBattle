6ef20faf7cb63a508ed89b967a20dac7a9823ac7
nam le hung <nam.lehung@gameloft.com>
2020-10-13 17:55:01 +0700

    + add UI

--- Assets/Scripts/Battle/GameController.cs
+++ Assets/Scripts/Battle/GameController.cs
@@ -1,23 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 public class GameController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int GameTimePerMatch;
     private static bool isFirstLaunch = true;
+    private static bool isGamePause = false;
+
+    private float totalTime;
+    private float timeLimit;
+    private TextMeshProUGUI timeRemain;
+    private TextMeshProUGUI txtGamePause;
+    // Start is called before the first frame update
     void Start()
     {
         if(GameController.isFirstLaunch) 
         {
-            //ARToggleButton.SetARstatus(SceneController.GotoARScene());
             GameController.isFirstLaunch = false;
         }
+        totalTime = 0;
+        timeLimit = GameTimePerMatch;
+        timeRemain = GameObject.Find("Canvas/Game_UI/Time/Timeremain").GetComponentInChildren<TextMeshProUGUI>();
+        txtGamePause = GameObject.Find("Canvas/Game_UI/txtGamePause").GetComponent<TextMeshProUGUI>();
+        txtGamePause.gameObject.SetActive(false);
+    }
+
+    public void CheckAndGotoAR()
+    {
+    #if PLATFORM_ANDROID
+        if(GamePermission.HasPermissionToUseAR())
+        {
+            SceneController.GotoARScene();
+        }
+    #endif
+    }
+    void OnApplicationFocus(bool hasFocus)
+    {
+        PauseGame(!hasFocus);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(isGamePause)
+            return;
+
+        totalTime += Time.deltaTime;
+        timeLimit -= Time.deltaTime;
+        timeRemain.text = Mathf.RoundToInt(timeLimit) + "s";
     }
 
-   
+    public static bool IsGamePause()
+    {
+        return isGamePause;
+    }
+   public void PauseGame(bool pause)
+   {
+       if(isGamePause != pause)
+       {
+           isGamePause = pause;
+           if(isGamePause)
+            {
+                txtGamePause.gameObject.SetActive(true);
+                Time.timeScale = 0;
+                AudioListener.pause = true;
+            }
+            else
+            {
+                txtGamePause.gameObject.SetActive(false);
+                Time.timeScale = 1;
+                AudioListener.pause = false;
+            }
+       }
+   }
 }
---
+++ Assets/Scripts/UI/EnergyController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnergyController : MonoBehaviour
+{
+    public float EnergyGeneratePerSec;
+    public float MaxEnergy = 6.0f;
+    private float currentEnergy;
+
+    // Start is called before the first frame update
+    private Slider fillbar;
+    private Slider filltrans;
+    void Start()
+    {
+        fillbar  = transform.GetComponent<Slider>();
+        filltrans  = transform.GetChild(2).GetComponent<Slider>();
+        currentEnergy = 0;
+    }
+
+    public float GetCurrentEnergy()
+    {
+        return currentEnergy;
+    }
+
+    public bool SpentEnergy(float value)
+    {
+        if(value < currentEnergy)
+        {
+            currentEnergy -= value;
+            return true;
+        }
+        return false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameController.IsGamePause())
+        {
+            return;
+        }
+        currentEnergy += EnergyGeneratePerSec*Time.deltaTime;
+        if(currentEnergy >= MaxEnergy)
+        {
+            currentEnergy = MaxEnergy;
+        }
+        SetFillBar();
+    }
+    public void SetFillBar()
+    {
+        float fillbarvalue = currentEnergy/MaxEnergy;
+        //Debug.Log("set fill bar: " + fillbarvalue);
+        fillbar.value = fillbarvalue;
+        int energy = (int)(currentEnergy);
+        filltrans.value = 1 - (energy < 3 ? energy*0.168f : energy*0.167f);
+    }
+}
